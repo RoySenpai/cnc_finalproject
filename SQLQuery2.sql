1add07e83e1b06fb545181c9443f1f6a54245e4f
Yuval <120040960+YuvalYurzd@users.noreply.github.com>
2023-02-23 12:48:23 +0200

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [First Name]
-      ,[Last name]
-      ,[ID]
-      ,[Work ID]
-      ,[Salary]
-  FROM [project].[dbo].[Workers]
