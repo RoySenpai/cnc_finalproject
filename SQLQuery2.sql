98bd57014c689fb1baf0efaf77c99a12b155a0c6
Yuval <120040960+YuvalYurzd@users.noreply.github.com>
2023-02-23 12:47:57 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [First Name]
+      ,[Last name]
+      ,[ID]
+      ,[Work ID]
+      ,[Salary]
+  FROM [project].[dbo].[Workers]
