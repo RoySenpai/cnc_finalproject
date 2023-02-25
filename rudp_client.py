8e1cc76d85cc46117cec3d5c06db87f566267523
Yuval <120040960+YuvalYurzd@users.noreply.github.com>
2023-02-25 15:08:54 +0200

    Update rudp_client.py

--- rudp_client.py
+++ rudp_client.py
@@ -1,17 +1,20 @@
 import socket
+import time
 
-# Define ip, port and max packet size
+# Define ip, ports, max packet size and congestion control variables
 server_port = 1234
 ip = "127.0.0.1"
-client_Port = 5000
+client_port = 5000
 max_Size = 1024
+timeout = 2
+max_Retries = 4
 
-# Create UDP socket to connect to server
+# Create a UDP socket to connect to the server
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((ip, client_Port))
+sock.bind((ip, client_port))
 
 # "Handshake" with the server
-message = "Hand shake"
+message = "Handshake"
 sock.sendto(message.encode('utf-8'), (ip, server_port))
 
 # Receive list of queries from the server
@@ -19,32 +22,52 @@ while True:
     try:
         data, server_address = sock.recvfrom(max_Size)
         print(f"Received from server {server_address}: {data.decode('utf-8')}")
+        sock.sendto("ACK".encode("utf-8"), (ip, server_port))
         break
     except socket.timeout:
         print("Timeout while waiting for queries")
 
 # Send queries to the server
+query_num = 1  # Query serial number
 while True:
+    sock.settimeout(timeout)
     query_name = input("Choose query to use, or type nothing to stop: ")
-    sock.sendto("ACK".encode("utf-8"), (ip, server_port))
-    sock.sendto(query_name.encode('utf-8'), (ip, server_port))
-
-    if query_name == "nothing":
-        break
-    data, address = sock.recvfrom(max_Size)
-    data = data.decode('utf-8')
-    
-    if data == "Timed out":
-        print("Server timed out, closing connection...")
-        break
-        
-    if data != "ACK":  # if server didn't return ACK for the first time, try to send again
+    query_name = query_name + "|" + str(query_num)
+    retries = 0
+    while retries < max_Retries:
+        start_time = time.time()  # Measure the start time of transmission
         sock.sendto("ACK".encode("utf-8"), (ip, server_port))
         sock.sendto(query_name.encode('utf-8'), (ip, server_port))
-        data, address = sock.recvfrom(max_Size)
-        data = data.decode('utf-8')
-        if data != "ACK":  # if server didn't return ACK for the second time, close the connection.
-            print("Didn't receive Ack from server for the second time, closing connection...")
-            break
-# Close socket
+
+        try:
+            data, address = sock.recvfrom(max_Size)
+            data = data.decode('utf-8')
+            if data == "ACK":
+                break
+            if data == "Timed out":
+                break
+        except socket.timeout:
+            pass  # Retry if timeout occurs
+
+        end_time = time.time()  # Measure the end time of transmission
+        rtt = end_time - start_time  # Calculate the RTT
+        if rtt < 0.5 * timeout:
+            timeout *= 0.9  # Increase the transmission rate
+        else:
+            timeout *= 1.2  # Decrease the transmission rate
+
+        retries += 1
+    query_num += 1
+    if "nothing" in query_name:
+        print("Closing connection...")
+        break
+    if retries == max_Retries:
+        print("Maximum number of retries reached, closing connection...")
+        break
+    elif data == "Timed out":
+        print("Server timed out, closing connection...")
+        break
+    else:
+        print(f"Received from server {server_address}: {data}")
+
 sock.close()
